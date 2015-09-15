7501bbabdb1f61922e80f24a3e50fb96b270097a
0910 <e@nuevediez.com.ar>
2015-09-15 11:05:18 -0300

    foreign keys fix

--- db/migrate/20150804173601_create_projects.rb
+++ db/migrate/20150804173601_create_projects.rb
@@ -2,15 +2,15 @@ class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
       t.string :title
-      t.references :project_type, index: true, foreign_key: true
+      t.references :project_type_id, index: true, foreign_key: true
       t.text :description
       t.text :benefit
       t.text :solved_problems
-      t.references :target, index: true, foreign_key: true
-      t.references :category, index: true, foreign_key: true
+      t.references :target_id, index: true, foreign_key: true
+      t.references :category_id, index: true, foreign_key: true
       t.string :link
-      t.references :city, index: true, foreign_key: true
-      t.references :user, index: true, foreign_key: true
+      t.references :city_id, index: true, foreign_key: true
+      t.references :user_id, index: true, foreign_key: true
 
       t.timestamps null: false
     end
