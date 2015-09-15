e6272231cf7e777de67be198bd61cff0f4f925ca
0910 <e@nuevediez.com.ar>
2015-09-15 17:52:17 -0300

    added nginx restart to deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
+      sudo 'service nginx restart'
     end
   end
 
