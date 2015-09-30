9c0ebb7fb6089844029c95ae66fd01f40293ef5b
0910 <e@nuevediez.com.ar>
2015-09-30 18:44:01 -0300

    Mobile and tablet fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,6 +20,18 @@ set :keep_releases, 5
 
 
 namespace :deploy do
+  
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
